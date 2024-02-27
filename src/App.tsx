ad0d8614dd39b7c90b0f8af42597a140c0306a78
Rehan <rehanadipurwana@gmail.com>
2024-02-27 20:49:11 +0700

    feat: add About page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/home"
+import About from "./pages/about"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   )
 }
